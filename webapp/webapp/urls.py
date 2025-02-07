d57743ef29cd0a9cf4de19a3555305fec42a9ee3
Rishabh Kothari <137794827+RishabhK103@users.noreply.github.com>
2025-02-07 19:55:59 +0530

    4th

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -18,9 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from firstapp import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
-    path('result/',views.result,name='result')
+    path('', views.index),
+    path('result/', views.result, name='result'),
 ]
